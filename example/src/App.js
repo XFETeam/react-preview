4aee97836b16a14b2c1c326e49c93278f538e612
browniu <niuniusoga@qq.com>
2019-07-02 20:26:23 +0800

    +

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-import ExampleComponent from 'react-preview'
+import Preview from 'react-preview'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Preview>
+          <div className="gallery">
+            <img src="https://farm4.staticflickr.com/3894/15008518202_b016d7d289_m.jpg" alt='preview' data-proto='https://farm4.staticflickr.com/3894/15008518202_c265dfa55f_h.jpg' />
+            <img src="https://farm4.staticflickr.com/3920/15008465772_383e697089_m.jpg" alt='preview' data-proto='https://farm4.staticflickr.com/3920/15008465772_d50c8f0531_h.jpg' />
+          </div>
+        </Preview>
       </div>
     )
   }
---
+++ source/pw/README.markdown
@@ -0,0 +1,4 @@
+# Multiple PhotoSwipe galleries on pagea
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/dimsemenov/pen/ZYbPJM](https://codepen.io/dimsemenov/pen/ZYbPJM).
+
+ 
